d8789af02755e461200763eae500cab01a6e5f82
fikrigoldbot <fikriteacher@gmail.com>
2025-05-15 20:59:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
-mport telebot
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-API_KEY = "7578653757:AAGyM99MMy_ffcP7FRLpvw6cL5zMiscACm8"
-bot = telebot.TeleBot(API_KEY)
+API_TOKEN = "7578653757:AAGyM99MMy_ffcP7FRLpvw6cL5zMiscACm8"
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Welcome! Gold signals bot is running.")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Welcome! Gold signals bot is running.")
 
-bot.polling()
+if _name_ == "_main_":
+    app = ApplicationBuilder().token(API_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot is running... Press Ctrl+C to stop.")
+    app.run_polling()
